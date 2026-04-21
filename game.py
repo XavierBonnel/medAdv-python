360828ccd9cc2035f8a46c03ab6e1ee28acd1e52
XavierBonnel <xavierbonnel@pm.me>
2026-04-21 22:31:58 +0200

    Add web terminal version: Pyodide + xterm.js static deployment
    Replaces the online paid service with a fully static HTML+Python solution.
    Python runs in-browser via WebAssembly (Pyodide). No server required.
    Deployable on GitHub Pages or Netlify as-is.
    
    - index.html: xterm.js terminal UI with Pyodide runtime and async input bridge
    - game.py: async-adapted game (input → await input, is → ==, termcolor → ANSI)

---
+++ game.py
@@ -0,0 +1,187 @@
+from pyfiglet import figlet_format
+
+RED   = '\033[1;31m'
+BLUE  = '\033[1;34m'
+RESET = '\033[0m'
+
+
+async def main():
+    print(RED + figlet_format("Medieval Adventures", font="slant") + RESET)
+
+    print("""
+|          .-----.     |
+| \\ ' /   _/    )/     |
+|- ( ) -('---''--).    |
+| / . \\(((()\\^_^/)().  |
+|  \\\\_\\ (()_)-((()().  |
+|   '- \\ )/\\._./(().   |
+|     '/\\/( X   ) \\    |
+|     (___)|___/ ) \\.  |
+|          |.#_|(___). |
+|         /\\    \\ ( (_ |
+|         \\/\\/\\/\\) \\\\.  |
+|         | / \\ |.     |
+|         |(   \\|.     |
+|        _|_)__|_\\_.   |
+|        )...()...(.   |
+|         | (   \\ |    |
+|      .-'__,)  (  \\. |
+|            '\\_-,     |
+             mrf""")
+
+    name = await input("\n\n What is your name adventurer?\n")
+    print("\nWelcome to this wonderful journey " + name + "\n")
+    print("where do you want to go?\n")
+    print("1. the dark forest of the elves?")
+    print("2. the cosey mines of the dwarves?\n")
+
+    first_choice = await input("choose: ")
+
+    if first_choice == "1":
+        print(r"""
+     | ^  ^  ^   ^      ___I_      ^  ^   ^  ^  ^   ^  ^  |
+     |/|\/|\/|\ /|\    /\-_--\    /|\/|\ /|\/|\/|\ /|\/|\ |
+     |/|\/|\/|\ /|\   /  \_-__\   /|\/|\ /|\/|\/|\ /|\/|\ |
+     ||/|\/|\/|\ /|\  |[]| [] |   /|\/|\ /|\/|\/|\ /|\/|\ |
+      ejm96
+        """)
+        print("\nYou enter a dark mysterious and ancient forest, you hear the wind and a distant song, probably a sad one\n")
+        print("What do you do?\n")
+        print("1. You walk toward the voice")
+        print("2. You run in the opposite direction\n")
+
+        second_choice = await input("choose: ")
+
+        if second_choice == "1":
+            print(r"""
+|:____|\_________b___/______.____.__|__________._______.---.__.____________:|
+|:____|/__4____|.____$______|____|__|__________|\__-,__|___|__|\_______..__:|
+|:___/|___4____'`____/______|____|__|__________|___/___|___|__|\___""______:|
+|:__('|)___________________@|___(|__|__#( )___@|._____@|__@|_@|____________:|
+     "|                              mp                                    /
+""")
+            print("You approach an old elf. He tells you about his youth as a great warrior in the century war he participated in against the orks to protect the forest of his ancestors.\n")
+
+        elif second_choice == "2":
+            print(r"""
+|                 ^                  |
+|                / \                 |
+|           ^   _|.|_   ^            |
+|         _|I|  |I .|  |.|_          |
+|         \II||~~| |~~||  /          |
+|          ~\~|~~~~~~~|~/~           |
+|             \|II I ..|/            |
+|        /\    |II.    |    /\.      |
+|       /  \  _|III .  |_  /  \.    |
+|       |-~| /(|I.I I  |)\ |~-|.    |
+|     _/(I | +-----------+ |. )\_.  |
+|     \~-----/____-~-____\-----~/.  |
+|      |I.III|  /(===)\  |  .. |.   |
+|      /~~~-----_________---~~~\.   |
+|     `##########!\-#####%!!!!!| |\ |
+|    _/###########!!\~~-_##%!!!\_/|.|
+|    \##############!!!!!/~~-_%!!!!\ |
+|     ~)#################!!!!!/~~--\_|
+|  __ /#####################%%!!!!/ /|
+|  \,~\-_____##############%%%!!!!\/ |
+|  /!!!!\ \ \~-_###########%%%!!!!\  |
+| /#####!!!!!!!\~-_#######%%%!!!!!!\_|
+|/#############!!!\#########%%%!!!!!!|\
+""")
+            print("\nyou ran through the woods past a hill and you see a magnificent castle beneath you\n")
+
+        else:
+            print("please choose 1 or 2 adventurer")
+
+    elif first_choice == "2":
+        print(r"""
+|                      _                                                      |
+|                     /#\                                                     |
+|                    /###\     /\                                             |
+|                   /  ###\   /##\  /\                                        |
+|                  /      #\ /####\/##\                                       |
+|                 /  /      /   # /  ##\             _       /\               |
+|               // //  /\  /    _/  /  #\ _         /#\    _/##\    /\.       |
+|              // /   /  \     /   /    #\ \      _/###\_ /   ##\__/ _\.      |
+|             /  \   / .. \   / /   _   { \ \   _/       / //    /    \\.     |
+|     /\     /    /\  ...  \_/   / / \   } \ | /  /\  \ /  _    /  /    \ /\.|
+|  _ /  \  /// / .\  ..%:.  /... /\ . \ {:  \\   /. \     / \  /   ___   /  \|
+| /.\ .\.\// \/... \.::::..... _/..\ ..\:|:. .  / .. \\  /.. \    /...\ /  \ \|
+|/...\.../..:.\. ..:::::::..:..... . ...\{:... / %... \\/..%. \  /./:..\__   \|
+| .:..\:..:::....:::;;;;;;::::::::.:::::.\}.....::%.:. \ .:::. \/.%:::.:..\.  |
+|::::...:::;;:::::;;;;;;;;;;;;;;:::::;;::{:::::::;;;:..  .:;:... ::;;::::..   |
+|;;;;:::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;];;;;;;;;;;::::::;;;;:.::;;;;;;;;:..|
+|;;;;;;;;;;;;;;ii;;;;;;;;;;;;;;;;;;;;;;;;[;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;|
+|;;;;;;;;;;;;;;;;;;;iiiiiiii;;;;;;;;;;;;;;};;ii;;iiii;;;;i;;;;;;;;;;;;;;;ii;;;|
+|iiii;;;iiiiiiiiiiIIIIIIIIIIIiiiiiIiiiiii{iiIIiiiiiiiiiiiiiiii;;;;;iiiilliiiii|
+|IIIiiIIllllllIIlllIIIIlllIIIlIiiIIIIIIIIIIIIlIIIIIllIIIIIIIIiiiiiiiillIIIllII|
+|IIIiiilIIIIIIIllTIIIIllIIlIlIIITTTTlIlIlIIIlIITTTTTTTIIIIlIIllIlIlllIIIIIIITT|
+|IIIIIIIIITTTTTTTTTTTTTIIIIIIIITTTTTTTTIIIIIITTTTTTTTTTTTTTIIIIIIIIIIIIIITTTTT|
+        """)
+        print("\nThe mines seem deserted at first sight but you can see a cozy fire far away\n")
+        print("What do you do?\n")
+        print("1. You run toward the fire — the closer you get, the stranger the atmosphere.")
+        print("2. You take your sword and walk carefully down a corridor.\n")
+
+        third_choice = await input("choose: ")
+
+        if third_choice == "1":
+            print(r"""
+|                  '                                      |
+|                  )                    `                 |
+|                 /(l                   /)                |
+|                (  \                  / (                |
+|                ) * )                ( , )               |
+|                 \#/                  \#'                |
+|               .-"#'-.             .-"#"=,               |
+|            (  |"-.='|            '|"-,-"|               |
+|            )\ |     |  ,        /(|     | /(         ,  |
+|   (       /  )|     | (\       (  \     | ) )       ((. |
+|   )\     (   (|     | ) )      ) , )    |/ (        ) \ |
+|  /  )     ) . )     |/  (     ( # (     ( , )      /   )|
+| ( * (      \#/|     (`# )      `#/|     |`#/      (  '( |
+|  \#/     .-"#'-.   .-"#'-,   .-"#'-.   .-=#"-;     `#/.|
+|.-"#'-.   |"=,-"|   |"-.-"|)  1"-.-"|   |"-.-"|   ,-"#"-.|
+||"-.-"|   |  !  |   |     |   |     |   |     !   |"-.-"||
+||     |   |     |._,|     |   |     |._,|     a   |     ||
+||     |   |     |   |     |   |     |   |     p   |     ||
+||     |   |     |   |     |   |     |   |     x   |     ||
+|'-._,-'   '-._,-'   '-._,-'   '-._,-'   '-._,-"   '-._,-'|
+""")
+            print("Finally, you meet a group of happy dwarves smoking and debating on how to get more gold out of the mountain. You share a beer with them and they invite you to the city a kilometer below.\n")
+
+        elif third_choice == "2":
+            print(r"""
+_____________________________________________
+|.'',                                     ,''.|
+|.'.'',                                 ,''.'.|
+|.'.'.'',                             ,''.'.'.|
+|.'.'.'.'',                         ,''.'.'.'.|
+|.'.'.'.'.|                         |.'.'.'.'.|
+|.'.'.'.'.|===;                 ;===|.'.'.'.'.|
+|.'.'.'.'.|:::|',             ,'|:::|.'.'.'.'.|
+|.'.'.'.'.|---|'.|, _______ ,|.'|---|.'.'.'.'.|
+|.'.'.'.'.|:::|'.|'|???????|'|.'|:::|.'.'.'.'.|
+|,',',',',|---|',|'|???????|'|,'|---|,',',',',|
+|.'.'.'.'.|:::|'.|'|???????|'|.'|:::|.'.'.'.'.|
+|.'.'.'.'.|---|','   /%%%\   ','|---|.'.'.'.'.|
+|.'.'.'.'.|===:'    /%%%%%\    ':===|.'.'.'.'.|
+|.'.'.'.'.|%%%%%%%%%%%%%%%%%%%%%%%%%|.'.'.'.'.|
+|.'.'.'.','       /%%%%%%%%%\       ','.'.'.'.|
+|.'.'.','        /%%%%%%%%%%%\        ','.'.'.|
+|.'.','         /%%%%%%%%%%%%%\         ','.'.|
+|.','          /%%%%%%%%%%%%%%%\          ','.|
+|;____________/%%%%%Spicer%%%%%%\____________;|
+""")
+            print('Soon the corridor reveals many choices — good... and probably bad.\n')
+
+        else:
+            print("please choose 1 or 2 adventurer")
+
+    else:
+        print("please choose 1 or 2 adventurer")
+
+    print(BLUE + figlet_format("To be continued...", font="slant") + RESET)
+
+
+await main()
